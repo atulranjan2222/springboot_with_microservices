b769f538bc545fb73ca6d56d0e775f059c923b93
tuttu <tuttu@192.168.1.143>
2020-05-10 13:55:30 +0530

    updated controller

--- src/main/java/com/example/demo/controller/DemoController.java
+++ src/main/java/com/example/demo/controller/DemoController.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -16,7 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.model.CustomerModel;
 import com.example.demo.service.CustomerService;
-
+@CrossOrigin("*")
 @RestController
 @RequestMapping("/demo")
 public class DemoController {
