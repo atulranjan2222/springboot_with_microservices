92d7435a1ef741343d47784c61e76fc718f854a7
tuttu <tuttu@192.168.1.8>
2020-08-29 13:18:24 +0530

    spring cloud client

--- src/main/java/com/example/demo/security/config/SecurityConfigurations.java
+++ src/main/java/com/example/demo/security/config/SecurityConfigurations.java
@@ -56,7 +56,7 @@ public class SecurityConfigurations  extends WebSecurityConfigurerAdapter{
 		// We don't need CSRF for this example
 		httpSecurity.csrf().disable()
 				// dont authenticate this particular request
-				.authorizeRequests().antMatchers("/demo/authenticate").permitAll().
+				.authorizeRequests().antMatchers("/demo/authenticate","/actuator/refresh").permitAll().
 				// all other requests need to be authenticated
 				anyRequest().authenticated().and().
 				// make sure we use stateless session; session won't be used to
